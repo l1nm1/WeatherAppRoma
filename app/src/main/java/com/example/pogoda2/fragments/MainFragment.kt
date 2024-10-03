a9a8e54c261efe59df74e9f1a30ec7d2c91bcbeb
l1nm1 <romalujden@gmail.com>
2024-10-03 15:29:56 +0900

    5 урок заработал

--- app/src/main/java/com/example/pogoda2/fragments/MainFragment.kt
+++ app/src/main/java/com/example/pogoda2/fragments/MainFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.pogoda2.R
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.pogoda2.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -18,6 +21,25 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
